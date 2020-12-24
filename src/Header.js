045f5361330bd34c0d22b23c3cb4bbd134770d40
Govind B Chandran <govindchandran.18cs052@mbcet.ac.in>
2020-12-24 21:42:09 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+
+        <header>
+            <h1>Keepy</h1>
+        </header>
+            
+        </div>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>hello world</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
