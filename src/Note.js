20bf39d655edf52309e384d1907d2bc09f4eddc2
Govind B Chandran <govindchandran.18cs052@mbcet.ac.in>
2020-12-25 21:07:34 +0530

    Add files via upload

---
+++ src/Note.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Note() {
+    return (
+        <div>
+
+        <h1>Title</h1>
+        <p> The contents of the note</p>
+            
+        </div>
+    )
+}
+
+export default Note
