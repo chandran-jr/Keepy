c951c59bd2db5d1b57e6dcadb7e35c792aa0416d
Govind B Chandran <govindchandran.18cs052@mbcet.ac.in>
2020-12-25 20:55:05 +0530

    Add files via upload

---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function footer() {
+    return (
+        <div>
+
+        <p>
+            
+        </p>
+            
+        </div>
+    )
+}
+
+export default footer
