a4bea27f2ec6e129adefea61a42c52166a2abd40
Govind B Chandran <govindchandran.18cs052@mbcet.ac.in>
2021-01-04 21:31:25 +0530

    Add files via upload

--- src/Note.js
+++ src/Note.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Note() {
+function Note(props) {
     return (
         <div className="note">
 
-        <h1>Title</h1>
-        <p> The contents of the note</p>
+        <h1>props.title</h1>
+        <p>props.content</p>
             
         </div>
     )
