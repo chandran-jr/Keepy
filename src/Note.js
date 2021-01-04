f185bf0c41e37b26432b72025776e77112a92f1d
Govind B Chandran <govindchandran.18cs052@mbcet.ac.in>
2021-01-04 21:35:29 +0530

    Add files via upload

--- src/Note.js
+++ src/Note.js
@@ -4,8 +4,8 @@ function Note(props) {
     return (
         <div className="note">
 
-        <h1>props.title</h1>
-        <p>props.content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
             
         </div>
     )
