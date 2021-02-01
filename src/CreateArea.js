32a1dd1728b19699fd994907d07e53d342ce496f
Govind B Chandran <govindchandran235@gmail.com>
2021-02-02 01:12:31 +0530

    Add files via upload

--- src/CreateArea.js
+++ src/CreateArea.js
@@ -10,8 +10,8 @@ const [note,setNote] = useState( {
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" placeholder="Title" value={note.title} />
+        <textarea name="content" placeholder="Take a note..." rows="3" value={note.content} />
         <button>Add</button>
       </form>
     </div>
