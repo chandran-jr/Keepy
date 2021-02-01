238173ea10188e5f851f94f86cde69033949b4e1
Govind B Chandran <govindchandran235@gmail.com>
2021-02-02 01:01:48 +0530

    Add files via upload

--- src/Note.js
+++ src/Note.js
@@ -5,7 +5,7 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>DELETE</button>
+      <button>DEL</button>
     </div>
   );
 }
