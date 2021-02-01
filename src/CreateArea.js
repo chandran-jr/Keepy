666bd4f495bdd166e7fe00e8f901dac02a810ef4
Govind B Chandran <govindchandran235@gmail.com>
2021-02-02 01:07:07 +0530

    Add files via upload

--- src/CreateArea.js
+++ src/CreateArea.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+
+const [note,setNote] = useState( {
+    title: "",
+    content: ""
+}
+);
   return (
     <div>
       <form>
