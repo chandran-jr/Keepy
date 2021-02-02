c81a1c919f107acc29be08657bff1c4ca2747176
Govind B Chandran <govindchandran235@gmail.com>
2021-02-02 13:31:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -16,16 +16,28 @@ const [notes,setNotes] = useState([]);
 
   }
 
+  function deleteNote(id){
+    setNotes(prevNotes =>{
+      return prevNotes.filter((noteItem, index)=>{
+        return index !== id;
+      });
+    });
+
+  }
+
   return (
     <div>
       <Header />
       <CreateArea 
         onAdd={addNote}
       />
-     { notes.map((noteItem) => {
+     { notes.map((noteItem, index) => {
        return <Note 
+          key={index}
+          id = {index}
           title= {noteItem.title}
           content={noteItem.content}
+          onDelete={deleteNote}
 
         />
        
--- src/Note.js
+++ src/Note.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Note(props) {
+
+    function handleClick() {
+        props.onDelete(props.id);
+
+    }
+
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>DEL</button>
+      <button onClick={handleClick}>DEL</button>
     </div>
   );
 }
